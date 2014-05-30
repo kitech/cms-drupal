a8ed64df5b8e109ff8f8cc19b0dd85cd7f0c9995
liuguangzhao <liuguangzhao@users.sf.net>
2014-05-30 17:49:15 +0800

    zexport up

--- tools/export_html.php
+++ tools/export_html.php
@@ -41,25 +41,9 @@ while ($row = $gstmt->fetch(PDO::FETCH_ASSOC)) {
     $body = $row['body'];
     $body = nl2br($body);
 
-    $indexes[] = array('nid'=>$nid, 'title'=>$title, 'date'=>$row['timestamp']);
-
-    $html = 
-"
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset='utf8'>
-<title>${title}</title>
-</head>
-<body>
-${body}
-</body>
-</html>
-";
-    
     $sdir = substr("${nid}", 0, 1);
-    @mkdir("temp/${sdir}");
-    file_put_contents("temp/${sdir}/node_${nid}.html", $html);
+
+    $indexes[] = array('nid'=>$nid, 'title'=>$title, 'date'=>$row['timestamp']);
 
     ////////////
     $smarty->assign('nid', $nid);
@@ -68,9 +52,9 @@ ${body}
     $smarty->assign('date', $row['timestamp']);
 
     $page_n = $smarty->fetch('page_n.html');
+    @mkdir("temp/${sdir}");
     file_put_contents("temp/${sdir}/node_${nid}.html", $page_n);
 }
-
 
 
 $chidxes = array_chunk($indexes, 20);
@@ -82,34 +66,28 @@ foreach ($chidxes as $idx => $rows) {
     $prev = $idx - 1;
     $next = $idx + 1;
     $total_pages = count($chidxes)-1;
-    $navbar = "<p><a href='page_0.html'>第一页</a><a href='page_${prev}.html'>上一页</a> <a href='page_${next}.html'>下一页</a> <a href='page_${total_pages}.html'>最后一页</a> </p> <br />";
-    $index_page = "<!DOCTYPE HTML><html><head><meta charset='utf8'><title></title></head><body> ${navbar}";
+    $idx_pname = "temp/page_${idx}.html";
+    $title = "QtChina | qtchina.github.io";
+
     foreach ($rows as $idx2 => $row) {
         $nid = $row['nid'];
-        $title = $row['title'];
-        $date = @date('Y-m-d H:i', $row['date']);
+        $date = date('Y-m-d H:i', $row['date']);
         $sdir = substr("${nid}", 0, 1);
         
-        $index_page .= "<p>${date} <a href='${sdir}/node_${nid}.html' target='_blank'>${title}</a></p>\n";
         $rows[$idx2]['url'] = "${sdir}/node_${nid}.html";
-        // $rows[$idx2]['url'] = "${sdir}/node_${nid}.html";
-        
     }
 
-    $index_page .= "${navbar}</body></html>";
-    
-    $idx_pname = "temp/page_${idx}.html";
-    file_put_contents($idx_pname, $index_page);
-
-
     ///////////
+    $smarty->assign('title', $title);
     $smarty->assign('curr_page', $idx);
     $smarty->assign('prev_page', $prev);
     $smarty->assign('next_page', $next);
     $smarty->assign('total_pages', $total_pages);
     $smarty->assign('rows', $rows);
+
     $page_index = $smarty->fetch('page_index.html');
     file_put_contents($idx_pname, $page_index);
+
 }
 
 echo "count: ${cnter} \n";
@@ -118,4 +96,9 @@ echo "pdo error info: " . var_export($dbh->errorInfo(), true) . "\n";
 
 var_dump($dbh);
 print_r($dbh);
+
+function goana()
+{
+
+}
 
